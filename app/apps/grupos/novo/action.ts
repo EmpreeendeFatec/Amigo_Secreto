06cb51829d004a953d7b1ca07413587586ab46cc
rafaelvieiramaximo <rafavmaximo@gmail.com>
2025-04-28 17:55:21 -0300

    fix: remove unnecessary console.log and ensure proper error handling in createGroup function

--- app/apps/grupos/novo/action.ts
+++ app/apps/grupos/novo/action.ts
@@ -1,6 +1,7 @@
 'use server';//roda no lado do servidor
 
 import { createClient } from "@/utils/supabase/server";
+import { group } from "console";
 
 export type CreateGroupState = {
     success: boolean | null;
@@ -24,6 +25,35 @@ export async function createGroup(_previousState: CreateGroupState, formData: Fo
     const emails = formData.getAll("email");
     const groupName = formData.get("group-name");
 
-    console.log(names, emails, groupName);
+    const {data: newGroup, error} = await supabase.from("groups").insert({
+        name: groupName,
+        owner_id: authUser.user.id,
+    })
+
+    .select ()
+    .single();//retorna um único objeto
+
+    if(error){
+        return {
+            success: false,
+            message: "Erro ao criar um grupo",
+        }
+    }
+
+   const participants = names.map((name, index) => ({
+    group_id: newGroup.id,
+    name,
+    email: emails[index],
+   }));
+
+   const {data: createdParticipants, error: participantsError} = await supabase.from("participants").insert(participants)
+   .select();
+
+   if(participantsError){
+    return {
+        success: false,
+        message: "Erro ao criar um grupo",
+    }
+   };
 
 }
