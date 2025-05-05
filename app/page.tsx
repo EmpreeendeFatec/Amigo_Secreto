b6c4b5235eea452fe7a191833855c545e999fb56
rafaelvieiramaximo <rafavmaximo@gmail.com>
2025-05-05 19:25:41 -0300

    feat: add cookie handling and state management for theme and cookie banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import LoginPage from "./(auth)/login/page";
+'use client';
 
-export default function Home() {
-  return <div className="bg-background w-screen h-screen"><LoginPage/></div>;
+import LoginPage from "./(auth)/login/page";
+import { cookies } from "next/headers";
+import { useState } from "react";
+
+export default  async function Home() {
+  
+  const cookieStore = await cookies();
+  const theme = cookieStore.get('theme');
+
+  const [showCookieBanner, setShowCookieBanner] = useState(false);
+
+  return <div className="bg-background w-screen h-screen">
+    <LoginPage/>
+    { showCookieBanner}</div>;
+
+  
 }
